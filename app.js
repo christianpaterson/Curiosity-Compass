8197c9d5c75aa9db70017f881ca0317266ceb9b5
Chris <cpaterson152@gmail.com>
2023-06-09 19:20:06 -0700

    Add Wikipedia API functionality

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 function initializeMaps() {
-  myMap();
+  // myMap();
   initMap();
 }
 
-function myMap() {
-  const mapProp = {
-    center: new google.maps.LatLng(-37.067549, -12.310659),
-    zoom: 5,
-  };
-  const map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
+// function myMap() {
+//   const mapProp = {
+//     center: new google.maps.LatLng(-37.067549, -12.310659),
+//     zoom: 5,
+//   };
+//   const map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+// }
 
 function initMap() {
   // Create a new map instance
@@ -23,3 +23,20 @@ function initMap() {
 window.addEventListener("load", function () {
   initializeMaps();
 });
+
+
+fetch('https://en.wikipedia.org/w/api.php?action=query&origin=*&prop=extracts&exintro&titles=Tristan_da_Cunha&format=json')
+  .then(response => response.json()) // Parse the response as JSON
+  .then(data => {
+    // Extract the first paragraph from the response
+    const pages = data.query.pages;
+    const pageId = Object.keys(pages)[0];
+    const extract = pages[pageId].extract;
+
+    // Display the first paragraph on your website
+    const paragraphElement = document.getElementById('article-container');
+    paragraphElement.innerHTML = extract;
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
